fa6d830ffaeae9afc60803f1eb6bc2fddcf57c5c
Tomasz Klekner <tomasz.klekner@gmail.com>
2021-12-03 08:25:06 +0100

    Add sourcemaps and min css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
+const sourcemaps = require("gulp-sourcemaps");
 
 function buildStyles() {
-  return src("sass/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("sass/**/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("css"));
 }
 
 function watchTask() {
